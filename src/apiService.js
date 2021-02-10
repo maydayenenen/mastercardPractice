f3d92cc636f56dfe8f6f49c765a1379db2eed365
Linfeng Wan <linfengwan@linfengs-mbp.myfiosgateway.com>
2021-02-09 22:52:49 -0500

    finished

--- src/apiService.js
+++ src/apiService.js
@@ -6,19 +6,10 @@ export const searchUser = (term) => {
   return axios.get(`${BASE_URL}/search/users`, {
     params: {
       q: term
-    },
-    auth: {
-      username: "zsdjl",
-      password: "3ad65cda509104976e33ab58476fee0a211abe75"
     }
   });
 };
 
 export const getReposByUser = (userName) => {
-  return axios.get(`${BASE_URL}/users/${userName}/repos`, {
-    auth: {
-      username: "zsdjl",
-      password: "3ad65cda509104976e33ab58476fee0a211abe75"
-    }
-  });
+  return axios.get(`${BASE_URL}/users/${userName}/repos`);
 };
