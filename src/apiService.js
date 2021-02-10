241b0861212c538c4c675a496cadb99755ed0a34
Linfeng Wan <linfengwan@linfengs-mbp.myfiosgateway.com>
2021-02-09 23:32:18 -0500

    comment out my token

--- src/apiService.js
+++ src/apiService.js
@@ -6,19 +6,19 @@ export const searchUser = (term) => {
   return axios.get(`${BASE_URL}/search/users`, {
     params: {
       q: term
-    },
+    } /** ,
     auth: {
       username: "maydayenenen",
       password: "b9baf8255b93923392747fab763205931ce0d103"
-    }
+    } */
   });
 };
 
 export const getReposByUser = (userName) => {
-  return axios.get(`${BASE_URL}/users/${userName}/repos`, {
+  return axios.get(`${BASE_URL}/users/${userName}/repos`/** , {
     auth: {
       username: "maydayenenen",
       password: "b9baf8255b93923392747fab763205931ce0d103"
     }
-  });
+  } */);
 };
